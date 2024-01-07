6d40c4ddfddfed15a5d107962b6fddb20efab106
noutcha <jonathannoutcha@gmail.com>
2024-01-08 00:25:15 +0100

    data type number in python

---
+++ Data type/numbers_1.py
@@ -0,0 +1,11 @@
+"""
+Numbers is one of the datatype of python language
+"""
+
+one_variable = 1
+second_variable = one_variable + 4
+print(second_variable)
+print(type(second_variable))
+another_variable = one_variable/second_variable
+print(another_variable)
+print(type(another_variable))
