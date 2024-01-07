6db20c6f281f9d440f98912da6651bc27d82fe8c
noutcha <jonathannoutcha@gmail.com>
2024-01-08 00:47:10 +0100

    dynamic typing and restrictions on data types

---
+++ Data type/dynamic_typing.py
@@ -0,0 +1,11 @@
+"""
+with python language it is possible to declare a variable whitout a datatype.
+the variable can change the type in the program
+"""
+
+variable = "Jonathan"
+print(variable)
+variable = "Dora"
+print(variable)
+variable = 8080
+print(variable)
--- Data type/operations_number.py
+++ Data type/operations_number.py
@@ -2,9 +2,12 @@
 + addition
 - substraction
 * multiplication
-/ division
+/ decimal division 
+// enteger division
 ** power
 () parenthese
 """
 variable_one = 3 + 4 * 8
+power = (variable_one/5)**3
 print(variable_one)
+print(power)
