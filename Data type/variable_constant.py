4e09a02c5eda7cfee7976f6072125b2250bd97a1
noutcha <jonathannoutcha@gmail.com>
2024-01-07 22:15:18 +0100

    variable and constant

---
+++ Data type/variable_constant.py
@@ -0,0 +1,7 @@
+"""
+A variable is a container that store something. Then it is use to store data
+"""
+variable_x = 5
+This_is_a_constant = "UIn Python, we cannot declare a variable or value as constant in Python. But we can indicate to programmers that a variable is a constant by using an all upper case variable name. Even though we can change the value, it is then the programmer's responsibility to leave this variable unchanged."
+print(variable_x)
+print(This_is_a_constant)
