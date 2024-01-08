641f9a3d5ab5c7a14145ecdf14f64196b81a89b2
noutcha <jonathannoutcha@gmail.com>
2024-01-08 04:44:39 +0100

    index and sciling

---
+++ Data type/sciling.py
@@ -0,0 +1,28 @@
+
+mon_string = "Je suis le meilleur"
+
+first_char = mon_string[0]
+print(first_char)
+
+third_char = mon_string[2]
+print(third_char)
+
+last_char = mon_string[-1]
+print(last_char)
+
+second_last_char = mon_string[-2]
+print(second_last_char)
+
+#string[start:stop:step]
+
+substring = mon_string[7:12]
+print(substring)
+
+partial_string = mon_string[:-5]
+print(partial_string)
+
+every_second_char = mon_string[::2]
+print(every_second_char)
+
+reversed_text = mon_string[::-1]
+print(reversed_text)
