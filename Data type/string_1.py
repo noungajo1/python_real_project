328b0eba29bb3e5307d9df7d38f61303e756664e
noutcha <jonathannoutcha@gmail.com>
2024-01-08 04:04:07 +0100

    data type string

---
+++ Data type/string_1.py
@@ -0,0 +1,12 @@
+"""
+tout ce qui est contenu dans les '', "" ou """"""" sont des string
+"""
+
+string = 'Je suis un string'
+print(type(string))
+string = "Je suis egalement un string"
+print(type(string))
+string = """
+Je suis un string qui peut se tenir sur plusieur lignes
+"""
+print(type(string))
