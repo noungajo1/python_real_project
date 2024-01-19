6148e8c610b289d5eafec1a024fc58ef51d18ed4
noutcha <jonathannoutcha@gmail.com>
2024-01-19 04:17:52 +0100

    arithmetic operators

---
+++ Data type/arithmetic_operators.py
@@ -0,0 +1,27 @@
+#Addition
+result = 5 + 3
+print(result)  #Output: 8
+
+#Subtraction
+result = 7 - 3
+print(result)  #Output: 4
+
+#Multiplication
+result = 6 * 3
+print(result)  #Output: 18
+
+#Division
+result = 10 / 3
+print(result)  #Output: 3.3333333333333333333
+
+#Floor division
+result = 10 // 3
+print(result)  #Output: 3
+
+#Modulus
+result = 10 % 3
+print(result)  #Output: 1
+
+#Exponentiation
+result = 5**3
+print(result)  #Output: 125
