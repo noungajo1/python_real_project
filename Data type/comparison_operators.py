5e1684b73857203ac3fad7485f42edf74211eedc
noutcha <jonathannoutcha@gmail.com>
2024-01-19 04:24:23 +0100

    comparison operator

---
+++ Data type/comparison_operators.py
@@ -0,0 +1,22 @@
+
+x = 5
+y = 10
+
+#Egality
+result = x == y  #False
+
+#Inegality
+result = x != y  #True
+
+#Greater than
+result = x > y #False
+
+#Less than
+result = x < y  #True
+
+#Greater than or equal
+result = x >= y  #True
+
+#Less than or equal
+result = x <= y  #True
+
