506ebccff5d96a278376c419411d8e9b5b84a482
noutcha <jonathannoutcha@gmail.com>
2024-01-21 02:17:26 +0100

    user input

---
+++ functions and methods/user_input.py
@@ -0,0 +1,8 @@
+x = input("i am the first variable")
+y = input("me, the second")
+
+z = x + y  # z is a string
+print(f"i am z : {z}")
+
+z = int(x) + int(y)
+print(f"i am z : {z}") # z is an integer
