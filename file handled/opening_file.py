c34f47729aea3ded8043c6080806571f73e2dd41
noutcha <jonathannoutcha@gmail.com>
2024-01-29 01:53:22 +0100

    reading from a file

--- file handled/opening_file.py
+++ file handled/opening_file.py
@@ -6,7 +6,18 @@ b for binary files for +
 
 #Open a file for reading : the file should exist
 file_path = "example.txt"
+#Read the entire content of the file
 with open(file_path,"r") as file:
     content = file.read()
     print(content)
 #File is automatically closed whrn exiting the with block
+
+#Read a single line from the file:the first line is read
+with open(file_path, "r") as file:
+    line = file.readline()
+    print(line)
+
+#Read all lines into a list
+with open(file_path, "r") as file:
+    lines = file.readlines()
+    print(lines)
