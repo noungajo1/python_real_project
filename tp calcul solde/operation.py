53cc8d62926a574418678222cc540b3f301fce1e
noutcha <jonathannoutcha@gmail.com>
2024-02-01 21:03:35 +0100

    nombre d'heure total pointe

--- tp calcul solde/operation.py
+++ tp calcul solde/operation.py
@@ -1,5 +1,5 @@
 import json
-
+from datetime import datetime
 
 #Read python object from a json file
 
@@ -9,7 +9,6 @@ def load_data_base():
     return data
 
 data_base = load_data_base()
-print(type(data_base))
 """
 je te fourni une date tu me donnes:
 - le nombre de jour valide durant cette semaine, 
@@ -17,11 +16,26 @@ je te fourni une date tu me donnes:
 - le nombre d'heure totale pointe
 format : aaaa/mm/jj
 """
+def get_hour_from_plage(plages):
+    time_in_seconde = 0
+    for plage in plages:
+        h1 = datetime.strptime(plage.split(" ")[0],'%H:%M')
+        h2 = datetime.strptime(plage.split(" ")[1],'%H:%M')
+        difference = (h2 - h1)
+        time_in_seconde = difference.seconds + time_in_seconde
+    return time_in_seconde/3600
+
+
+
 def get_plage_horaire(jours):
+    heure_total_pointe = 0
     for jour in jours:
         print(f"{jour['libelle_jour']} : ")
-        list_plage = jour["plage"]
+        list_plage = jour["plages"]
         print(list_plage)
+        heure_total_pointe = get_hour_from_plage(list_plage) + heure_total_pointe  
+    print(f'Le nombre d\heures valide est : {heure_total_pointe:.2f}') 
+    return 0
 
 
 def load_week_history_from_date(week_date):
@@ -37,9 +51,10 @@ def load_week_history_from_date(week_date):
                 print("Je n'ai pas travailler cette semaine")
             return 0
     print('Date non prise en charge')
+    return 0
 
     
     
 
-load_week_history_from_date('2024/01/29')
+load_week_history_from_date('2024/01/22')
 
